6c0389baeb3999ec3d6cebff9d89abf242d554ee
ShahnawazHussain28 <shahnawazhussain2802@gmail.com>
2023-03-30 00:57:15 +0530

    added patrolling behaviour

--- game/entities.py
+++ game/entities.py
@@ -22,6 +22,8 @@ class Entity(arcade.Sprite):
         #     self.walk_textures.append(texture)
         self.texture = self.idle_texture
         self.set_hit_box(self.texture.hit_box_points)
+    def get_position(self):
+        return Vec2(self.center_x, self.center_y)
 
 
 class Player(Entity):
@@ -54,21 +56,37 @@ class Player(Entity):
     def show(self):
         self.draw()
 
-    def get_position(self):
-        return Vec2(self.center_x, self.center_y)
-
 
 class Enemy(Entity):
-    def __init__(self, player):
+    def __init__(self, player, barrier_list):
         super().__init__("slimeBlock.png")
-        self.center_x, self.center_y = Vec2(SCREEN_WIDTH / 2 + 100, SCREEN_HEIGHT / 2)
         self.angle = -90
         self.speed = 3.0
         self.player = player
+        self.barrier_list = barrier_list
+        start = Vec2(300, 900)
+        end = Vec2(1200, 1280)
+        self.patrol_path = self.calculate_path(start, end)
+        self.patrol_path_index = 0
+        self.center_x, self.center_y = self.patrol_path[self.patrol_path_index]
+        self.state = "patrolling"
+        self.moving_forward = True
 
+    def calculate_path(self, start, end):
+        return arcade.astar_calculate_path(start, end, self.barrier_list, True)
+        
     def update(self):
-        # Calculate direction to the player
-        direction = self.player.get_position() - Vec2(self.center_x, self.center_y)
+        if self.state == "patrolling":
+            # Patrolling
+            point = self.patrol_path[self.patrol_path_index]
+            direction = Vec2(point[0], point[1]) - self.get_position()
+            if direction.mag < 5:
+                self.patrol_path_index += 1 if self.moving_forward else -1
+                if self.patrol_path_index == len(self.patrol_path)-1: self.moving_forward = False
+                elif self.patrol_path_index == 0: self.moving_forward = True
+        elif self.state == "chasing":
+            # Calculate direction to the player
+            direction = self.player.get_position() - self.get_position()
         if direction.mag != 0:
             direction = direction.normalize()
             self.angle = math.degrees(direction.heading)
