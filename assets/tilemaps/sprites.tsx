2329641ac192dc03cec635259f608485f7bd5408
ificiana <ificiana@gmail.com>
2023-03-30 21:33:30 +0530

    rework level map

--- assets/tilemaps/sprites.tsx
+++ assets/tilemaps/sprites.tsx
@@ -1,11 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="sprites" tilewidth="313" tileheight="207" tilecount="7" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="sprites" tilewidth="64" tileheight="64" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../sprites/wall.png"/>
- </tile>
- <tile id="1">
-  <image width="128" height="128" source="../sprites/enemy.png"/>
  </tile>
  <tile id="3">
   <image width="32" height="16" source="../sprites/arrow-down.png"/>
@@ -19,7 +16,10 @@
  <tile id="6">
   <image width="32" height="16" source="../sprites/arrow-up.png"/>
  </tile>
- <tile id="8">
-  <image width="313" height="207" source="../sprites/mc_idle.png"/>
+ <tile id="11">
+  <image width="64" height="64" source="../sprites/enemy.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="42" source="../sprites/mc_idle.png"/>
  </tile>
 </tileset>
