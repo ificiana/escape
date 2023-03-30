f33ddb73fbde870af6938416f4928cecfa508e97
ificiana <ificiana@gmail.com>
2023-03-30 20:57:01 +0530

    rework tiles and sprites

--- assets/tilemaps/sprites.tsx
+++ assets/tilemaps/sprites.tsx
@@ -1,7 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="sprites" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="sprites" tilewidth="313" tileheight="207" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../sprites/wall.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../sprites/enemy.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="16" source="../sprites/arrow-down.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="32" source="../sprites/arrow-left.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="32" source="../sprites/arrow-right.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="16" source="../sprites/arrow-up.png"/>
+ </tile>
+ <tile id="8">
+  <image width="313" height="207" source="../sprites/mc_idle.png"/>
  </tile>
 </tileset>
