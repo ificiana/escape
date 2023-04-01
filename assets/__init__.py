8cc7497c4b06aed455ced3ea94abfd3bc35f0bce
Vagish Vela <git@vagish.com>
2023-04-01 09:18:47 -0400

    remove shaders from assets

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,3 +1,3 @@
-from .manager import sprites, tilemaps, sounds, fonts, items, shaders
+from .manager import sprites, tilemaps, sounds, fonts, items
 
-__all__ = ["sprites", "tilemaps", "sounds", "fonts", "items", "shaders"]
+__all__ = ["sprites", "tilemaps", "sounds", "fonts", "items"]
